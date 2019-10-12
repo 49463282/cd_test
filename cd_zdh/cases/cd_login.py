d77f5f2f90258a62086adc1c5c7ae7ba9c013f64
49463282 <49463282@qq.com>
2019-10-12 11:16:28 +0800

    修改token

--- cd_zdh/cases/cd_login.py
+++ cd_zdh/cases/cd_login.py
@@ -21,7 +21,7 @@ class Cd_login(unittest.TestCase):
     def test_login(self):
         driver = self.driver
         driver.add_cookie({"name": "Admin-Token",
-                           "value": "1f7ddbea-7932-4c81-a427-1c45a97bf9cd"})
+                           "value": "3eb1e59b-0bc7-45eb-baf9-e0b7c49af721"})
         driver.refresh()
         gs = driver.find_element_by_class_name("store-detail").text
         zh = driver.find_element_by_xpath(
